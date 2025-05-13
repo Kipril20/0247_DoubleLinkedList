d200ae697417e212b7b18e744d055cb36960d10c
Kipril20 <rafky2004@gmail.com>
2025-05-13 10:40:27 +0700

    kondisi jika start tidak memiliki nilai

--- 0247_DoubleLinkedList.cpp
+++ 0247_DoubleLinkedList.cpp
@@ -39,3 +39,8 @@ void addNode()
         }
         // jika list kosong, maka node next nya adalah START
         newNode->next = START; // step 3: make the new node point to the first node
+        // kondisi jika start tidak memiliki nilai atau tidak kosong
+        if (START != NULL)
+        {
+            START->prev = newNode; // step 4: make the first node point to the node
+        }
