2dbc9a718bfac3c955b668045aeac674a83efc50
Kipril20 <rafky2004@gmail.com>
2025-05-13 11:02:40 +0700

    kondisi jika previous tidak sama dengan null

--- 0247_DoubleLinkedList.cpp
+++ 0247_DoubleLinkedList.cpp
@@ -73,3 +73,9 @@ void addNode()
         {
             current->prev = newNode; // step 6: Make the previous field of the
         }
+
+        // kondisi jika previous tidak sama dengan null
+        if (previous != NULL)
+        {
+            previous->next = newNode; // step 7: Make the next field of the previous
+        }
